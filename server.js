1da245888c8a633cfa3ec7033998e68d83aa261e
Zverloff <toddzverloff@gmail.com>
2018-10-29 23:28:49 -0400

    server file started, survey html in progress

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser')
+
 var app = express()
 
 var PORT = process.env.PORT || 3000
 
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve(__dirname, 'home.html'))
+})
+
+app.get('/survey', function(req, res){
+    res.sendFile(path.resolve(__dirname, 'survey.html'))
+})
+
+app.get('/api/friends', function(req, res) {
+    return res.json(friends)
+})
+
+app.post('/api/friends', function(req, res) {
+    var newFriend = req.body
+    res.json(newFriend)
+})
 
 
-app.listen(PORT)
+app.listen(PORT, function() {
+    console.log('Listening on port: ' + PORT)
+})
